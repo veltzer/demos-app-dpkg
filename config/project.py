fafb3370965ce113d3b07d3c7e1759c1ed86bc4c
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:55 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-dpkg"
-project_long_description = "Demos for the dpkg system"
-project_year_started = 2015
-project_description = """Demos for the dpkg system"""
+description_short = "Demos for the dpkg system"
